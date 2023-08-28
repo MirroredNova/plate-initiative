8af5bb9385a3ad53e1aac11a028804dc86cbb8ba
Nate Wiltzius <novawiltzius@gmail.com>
2023-08-28 01:15:27 -0500

    Project setup with font families and color variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      alice: ["var(--font-alice)"],
+      sofia: ["var(--font-sofia)"],
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'primary': 'var(--primary)',
+        'secondary-dark': 'var(--secondary-dark)',
+        'secondary-mid': 'var(--secondary-mid)',
+        'secondary-light': 'var(--secondary-light)',
+        'accent-primary-hover': 'var(--accent-primary-hover)',
+        'accent-secondary': 'var(--accent-secondary)',
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
