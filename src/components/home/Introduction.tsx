44f033fda6fe4e852093d587513b24abe4a11515
Nate Wiltzius <novawiltzius@gmail.com>
2023-08-31 19:10:52 -0500

    mobilized introduction and quote

--- src/components/home/Introduction.tsx
+++ src/components/home/Introduction.tsx
@@ -2,13 +2,13 @@ import Link from "next/link";
 import React from "react";
 
 const Introduction = () => (
-  <section className="flex justify-center">
-    <div className="flex px-12 py-20 max-w-5xl gap-4">
-      <div className="flex flex-col p-4">
+  <section className="flex justify-center pt-16 lg:mt-0">
+    <div className="flex flex-col lg:flex-row px-3 sm:px-12 py-20 max-w-5xl gap-4">
+      <div className="flex flex-col p-2 lg:p-4">
         <h3 className="text-accent_primary text-base font-extrabold font-sofia tracking-wide">
           OUR OBJECTIVE:
         </h3>
-        <h1 className="text-secondary_dark text-9xl font-bold mb-20">
+        <h1 className="text-secondary_dark text-3xl sm:text-9xl font-bold mb-4 sm:mb-20">
           P.L.A.T.E.
         </h1>
         <p className="font-alice text-lg">
@@ -17,8 +17,8 @@ const Introduction = () => (
           acquire essential life skills.
         </p>
       </div>
-      <div className="my-auto p-4">
-        <div className="text-accent_secondary bg-accent_primary rounded-full px-12 py-5 font-semibold min-w-max">
+      <div className="my-auto p-2 lg:p-4">
+        <div className="text-accent_secondary bg-accent_primary rounded-full px-12 py-5 font-semibold w-fit whitespace-nowrap">
           <Link href="/plate">Learn More</Link>
         </div>
       </div>
--- src/components/home/Quote.tsx
+++ src/components/home/Quote.tsx
@@ -8,7 +8,7 @@ type Props = {
 const Quote = ({ text, by }: Props) => (
   <section className="flex justify-center">
     <div className="flex px-12 py-20 max-w-5xl gap-4 flex-col text-center font-sofia">
-      <h2 className="font-extrabold text-secondary_dark text-4xl px-4 py-2 italic leading-[48px]">
+      <h2 className="font-extrabold text-secondary_dark text-3xl lg:text-4xl/[48px] px-4 py-2 italic">
         {`"${text}"`}
       </h2>
       <p className="px-4 py-2 text-accent_primary font-extrabold tracking-wider text-base">
